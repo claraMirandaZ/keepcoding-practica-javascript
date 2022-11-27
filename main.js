1c76a896235c1628a9eb66b86dcc16f35ce2d38a
miranda-zapata <miranda.zapata@gmail.com>
2022-11-27 11:40:12 +0100

    Add instructions' function
    So the user knows what to ask for

--- main.js
+++ main.js
@@ -29,6 +29,26 @@ import readline from 'readline';
 /*
 Cuando ejecutemos la aplicación, aparecerá el listado completo de requisitos. El usuario debe pulsar el número correspondiente a ese requisito para que se ejecute. Una vez la ejecución termine, volveremos a mostrar el listado de requisitos para que el usuario pueda seleccionar otro. En caso de que el usuario pulse el 0 o un número no contemplado, la aplicación terminará.
 */
+function instructions() {
+  console.log(
+    '¿Qué quieres ejecutar?\n',
+    '1. Mostrar todos los alumnos en formato de tabla.\n',
+    '2. Mostrar por consola la cantidad de alumnos que hay en clase.\n',
+    '3. Mostrar por consola todos los nombres de los alumnos.\n',
+    '4. Eliminar el último alumno de la clase.\n',
+    '5. Eliminar de la clase un alumno aleatorio.\n',
+    '6. Mostrar por consola todos los datos de los alumnos que son chicas.\n',
+    '7. Mostrar por consola el número de chicos y chicas que hay en la clase.\n',
+    '8. Mostrar true o false por consola si todos los alumnos de la clase son chicas.\n',
+    '9. Mostrar por consola los nombres de los alumnos que tengan entre 20 y 25 años.\n',
+    '10. Añadir un alumno nuevo con los siguientes datos: nombre aleatorio, edad aleatoria entre 20 y 50 años, género aleatorio y listado de calificaciones vacío.\n',
+    '11. Mostrar por consola el nombre de la persona más joven de la clase.\n',
+    '12. Mostrar por consola la edad media de todos los alumnos de la clase.\n',
+    '13. Mostrar por consola la edad media sólo de las chicas de la clase.\n',
+    '14. Añadir nueva nota a los alumnos. Por cada alumno de la clase tendremos que calcular una nota de forma aleatoria (entre 0 y 10) y añadirla a su listado de notas.\n',
+    '15. Ordenar el array de alumnos alfabéticamente, según su nombre.\n'
+  );
+}
 
 // Configuración de la utilidad de node.js para que los datos se pidan y se muestren por consola
 const rl = readline.createInterface({
@@ -44,7 +64,7 @@ function getUserNumber() {
       if (parseInt(num)) {
         resolve(num);
       } else {
-        reject('error');
+        reject('Por favor, introduce una opción válida');
       }
     });
   });
@@ -55,6 +75,8 @@ async function main() {
   let action;
   do {
     try {
+      // Si quisiéramos que el enunciado sólo apareciera una vez, al iniciar la aplicación, sacaríamos esta función del bucle
+      instructions();
       action = await getUserNumber();
     } catch (error) {
       console.log(error);
